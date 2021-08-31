62b6c752aa6df2e05a4fd493805283b97beb4a1c
fvig2001 <31123547+fvig2001@users.noreply.github.com>
2021-08-31 16:47:13 +0800

    Update encode.py
    Updated encoding script to support normalization and more file types

--- videos/encode.py
+++ videos/encode.py
@@ -2,7 +2,9 @@ import os
 
 newFiles = []
 directory = os.path.dirname(os.path.realpath(__file__))
+toencdirectory = os.path.join(directory, 'ToEncode')
 destinationDirectory = os.path.join(directory, 'encoded')
+normDirectory = os.path.join(directory, 'normalized')
 
 if not os.path.exists(destinationDirectory):
 	os.mkdir(destinationDirectory)
@@ -17,6 +19,12 @@ def isVideo(videofile):
 		return True
 	if videofile.lower().endswith('.avi'):
 		return True
+	if videofile.lower().endswith('.mpg'):
+		return True
+	if videofile.lower().endswith('.mpeg'):
+		return True
+	if videofile.lower().endswith('.wmv'):
+		return True
 	return False
 
 newFiles = [os.path.join(dp, f) for dp, dn, filenames in os.walk(directory) for f in filenames if isVideo(f)]
@@ -27,8 +35,13 @@ for filepath in newFiles:
 	newFile = '%s.mp4' % videoName
 	i = filepath
 	o = os.path.join(destinationDirectory, newFile)
-	if os.path.isfile(o):
+	n = os.path.join(normDirectory, newFile)
+	if os.path.isfile(n):
 		continue
-	encodeCommand = 'ffmpeg -i "%s" -vf scale=-1:480 -c:v libx264 -profile:v baseline -level 3.0 -preset fast -crf 23 -pix_fmt yuv420p "%s"' % (i, o)
+	encodeCommand = 'ffmpeg -y -i "%s" -vf scale=640:480 -c:v libx264 -profile:v baseline -level 3.0 -preset faster -crf 23 -pix_fmt yuv420p "%s"' % (i, o)
 	print('Encoding %s' % newFile)
 	encode = os.popen(encodeCommand).read()
+	print ('Normalize %s' % video)
+	normCommand = 'ffmpeg-normalize "%s" -ext mp4 -c:a aac' % (o)
+	norm = os.popen(normCommand).read()
+	os.remove(o)
