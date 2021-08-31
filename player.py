c499fe8208fd4d1de72fe01af8c0e9b98957632b
fvig2001 <31123547+fvig2001@users.noreply.github.com>
2021-08-31 16:52:28 +0800

    Update player.py
    Add support to load all folders

--- player.py
+++ player.py
@@ -8,17 +8,38 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(25, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 directory = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'videos')
-
+directoryS = os.path.join(directory, 'The Simpsons')
+simpsonsOnly = False
 videos = []
 
+
+def isVideo(videofile):
+	if videofile.lower().endswith('.mp4'):
+		return True
+	if videofile.lower().endswith('.mkv'):
+		return True
+	if videofile.lower().endswith('.mov'):
+		return True
+	if videofile.lower().endswith('.avi'):
+		return True
+	if videofile.lower().endswith('.mpg'):
+		return True
+	if videofile.lower().endswith('.mpeg'):
+		return True
+	if videofile.lower().endswith('.wmv'):
+		return True
+	return False
 
 def getVideos():
     global videos
     videos = []
-    for file in os.listdir(directory):
-        if file.lower().endswith('.mp4'):
-            videos.append(os.path.join(directory, file))
-
+    vidpath = directory
+    if (simpsonsOnly):
+        vidpath = simpsonsOnly
+    for currentpath, folders, files in os.walk(vidpath):
+        for file in files:
+            if isVideo(file):
+               videos.append(os.path.join(directory, file))
 
 def playVideos():
     global videos
