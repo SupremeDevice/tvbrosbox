a0bf0117de88c2ab72afc1a336cfd5e22a26c824
fvig2001 <31123547+fvig2001@users.noreply.github.com>
2021-09-26 12:47:31 +0800

    Add files via upload
    Add new feature to skip 15 seconds if button is pressed for more than 3 seconds. Fix issue where changing between directories causes the first file to be always played

--- player.py
+++ player.py
@@ -23,7 +23,6 @@ isLCD = True
 directory = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'videos')
 
 directorySimpsons = os.path.join(directory, 'Simpsons')
-myThread = None
 videos = []
 player = None
 isScreenOn = True
@@ -32,11 +31,13 @@ isWide = False #if false and not letterbox = zoom
 isSimpsons = False
 isCommentary = False
 isLetterBox = True
+timeLeft = 0
+nextHeldCnt = 0
 curIndex = -1 #index of video being played for 
 screenChanged = False #if screen mode changed
 stopPlayer = False
 mountPaths = ['/mnt/usb1','/mnt/usb2','/mnt/usb3','/mnt/usb4','/mnt/usb5','/mnt/usb6','/mnt/usb7','/mnt/usb8']
-
+adjustedTime = 0
 def getVideos():
 	global directory
 	global directorySimpsons
@@ -73,17 +74,21 @@ def getVideos():
 def endWait(length):
 	global stopPlayer
 	forced = False
+	global timeLeft
 	timeLeft = int(length)
 	origTimeLeft = 0
 	firstT = datetime.now()
 	lastT = datetime.now()
 	delta = lastT - firstT
 	stopped = False
-	global myThread
-	while delta.seconds < timeLeft and forced == False:
+	global adjustedTime
+	adjustedTime = 0
+	tLeft = delta.seconds
+	while tLeft < timeLeft and forced == False:
 		time.sleep(1)
 		lastT = datetime.now()
 		delta = lastT - firstT
+		tLeft = delta.seconds - adjustedTime
 		tempCmd = 'sudo ps -aux| grep omxplayer.bin'
 		#check if omxplayer is still running
 		try:
@@ -101,10 +106,6 @@ def endWait(length):
 		if stopPlayer:
 			#print("stopPlayer detected")
 			forced = True
-#		if not myThread.is_alive():
-#			myThread = threading.Thread(target=buttonWatcher)
-#			myThread.start()
-
 
 def playVideos():
 	global videos
@@ -114,9 +115,13 @@ def playVideos():
 	global isLetterbox
 	global isCommentary
 	global isSimpsons
+	global nextHeldCnt
 	curIndex = 0
 	vLength = 0
 	global player
+	rng = random.SystemRandom()
+	seedV = rng.randint(0, 65535)
+	random.seed(seedV)
 	if len(videos) == 0:
 		getVideos()
 		time.sleep(5)
@@ -130,6 +135,7 @@ def playVideos():
 		tempCmd = 'ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "%s"' % video
 		value =  check_output(tempCmd, shell=True)
 		vLength = int(float(value))
+		nextHeldCnt = 0
 		if isLetterBox:
 			player = OMXPlayer(video,args='--no-osd --aspect-mode Letterbox')
 		elif isWide:
@@ -144,7 +150,7 @@ def playVideos():
 			player.set_volume(1.25)
 
 		endWait(vLength - 6)
-		curIndex += 1
+		curIndex = curIndex + 1
 		if stopPlayer:
 			stopPlayer = False
 			if player is not None:
@@ -152,7 +158,8 @@ def playVideos():
 				player = None
 			#curIndex = len(videos) + 1
 			if length != len(videos): #changed mode
-				curIndex = 0
+				length = len(videos)
+				curIndex = length+1
 			
 def setup():
 	devPaths = ['/dev/sda','/dev/sdb','/dev/sdc','/dev/sdd','/dev/sda1','/dev/sdb1','/dev/sdc1','/dev/sdd1',]
@@ -344,10 +351,24 @@ def checkScreenOn():
 def checkNext():
 	global stopPlayer
 	global initDone
+	global player
+	global adjustedTime
 	input = not GPIO.input(6)
-	
-	if input and initDone:
-		stopPlayer = True	
+	global nextHeldCnt
+	compute = 0
+	if initDone:
+		if input:
+			nextHeldCnt = nextHeldCnt + 1
+			computeLeft = nextHeldCnt % 5
+			if nextHeldCnt > 15 and player is not None: #if 3 seconds have occurred and button is still pressed
+				if timeLeft > 21 and computeLeft == 0:
+					player.seek(15)
+					adjustedTime = adjustedTime + 15
+		else:
+			if nextHeldCnt > 0 and nextHeldCnt < 15: #normal skip
+				stopPlayer = True
+			nextHeldCnt = 0
+
 
 checkScreenOn()
 setup()
